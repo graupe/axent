fab7aa4c2d1d2232c9107e5a54ac3623f0761c5a
Stefan Schulze <stefan.andre.schulze@posteo.de>
2025-03-05 22:47:47 +0100

    Add more fail-saves to `AxentDef`

--- test/axent_def_test.exs
+++ test/axent_def_test.exs
@@ -1,6 +1,7 @@
 defmodule AxentDefTest do
   use ExUnit.Case
-  import ExUnit.CaptureIO
+  doctest AxentDef, import: true
+  # the module is only created within the test
   @compile {:no_warn_undefined, [AxentDefTest.Test]}
 
   setup do
@@ -17,7 +18,7 @@ defmodule AxentDefTest do
       defmodule AxentDefTest.Test do
         use AxentDef
 
-        def test_ok do
+        def test do
           {:ok, value} <- {:ok, :some_value}
           value
         else
@@ -25,12 +26,85 @@ defmodule AxentDefTest do
         end
       end
   """
+
+  @test_ok_complex_result ~S"""
+      defmodule AxentDefTest.Test do
+        use AxentDef
+
+        def test do
+          {:ok, value} <- {:ok, 4}
+          if value > 3 do
+            :too_large
+          else
+            value
+          end
+        else
+          _ -> :error
+        end
+      end
+  """
+  @test_ok_complex_intermediate ~S"""
+      defmodule AxentDefTest.Test do
+        use AxentDef
+
+        def test do
+          {:ok, value} <- {:ok, 4}
+          value = if value > 3 do
+            :too_large
+          else
+            value
+          end
+          value
+        else
+          _ -> :error
+        end
+      end
+  """
+  @test_ok_no_else ~S"""
+      defmodule AxentDefTest.Test do
+        use AxentDef
+
+        def test do
+          {:ok, value} <- {:ok, :some_value}
+          value
+        end
+      end
+  """
   @test_ok_with_warning ~S"""
       defmodule AxentDefTest.Test do
         use AxentDef
 
-        def test_warning_ok do
+        def test do
+          {:ok, _value} <- {:ok, :some_value}
+        else
+          _ -> :error
+        end
+      end
+  """
+
+  @test_ok_with_catch_discurage ~S"""
+      defmodule AxentDefTest.Test do
+        use AxentDef
+
+        def test do
           {:ok, value} <- {:ok, :some_value}
+          :ok
+        catch
+          _ -> raise "SomeError"
+        else
+          _ -> :error
+        end
+      end
+  """
+  @test_ok_with_rescue_discurage ~S"""
+      defmodule AxentDefTest.Test do
+        use AxentDef
+
+        def test do
+          {:ok, _value} <- {:ok, :some_value}
+          :ok
+        rescue
+          _ -> raise "SomeError"
         else
           _ -> :error
         end
@@ -40,7 +114,7 @@ defmodule AxentDefTest do
       defmodule AxentDefTest.Test do
         use AxentDef
 
-        def test_error do
+        def test do
           {:ok, value} <- {:error, {:some_reason, "message"}}
           value
         else
@@ -51,6 +125,7 @@ defmodule AxentDefTest do
   @test_normal_def ~S"""
     defmodule AxentDefTest.Test do
       use AxentDef
+      def normal_fn
       def normal_fn, do: :original
 
       def normal_fn_with_rescue do
@@ -69,28 +144,55 @@ defmodule AxentDefTest do
     end
   """
 
-  test "axent def" do
-    Code.compile_string(@test_ok)
-    assert AxentDefTest.Test.test_ok() == :some_value
+  describe "Axent extended behaviour" do
+    test "axent def" do
+      Code.compile_string(@test_ok)
+      assert AxentDefTest.Test.test() == :some_value
+    end
+
+    test "axent def complex result" do
+      Code.compile_string(@test_ok_complex_result)
+      assert AxentDefTest.Test.test() == :too_large
+    end
+
+    test "axent def complex intermediate" do
+      Code.compile_string(@test_ok_complex_intermediate)
+      assert AxentDefTest.Test.test() == :too_large
+    end
+
+    test "axent def no else" do
+      Code.compile_string(@test_ok_no_else)
+      assert AxentDefTest.Test.test() == :some_value
+    end
+
+    test "axent def error on implicit nil return" do
+      assert_raise SyntaxError, ~r"Expected non-match assignment as last expression", fn ->
+        Code.compile_string(@test_ok_with_warning)
+      end
+    end
+
+    test "axent def error on `<-` with `rescue` or `catch`" do
+      assert_raise SyntaxError, ~r"Don't mix `rescue` blocks and Axent syntax", fn ->
+        Code.compile_string(@test_ok_with_rescue_discurage)
+      end
+
+      assert_raise SyntaxError, ~r"Don't mix `catch` blocks and Axent syntax", fn ->
+        Code.compile_string(@test_ok_with_catch_discurage)
+      end
+    end
+
+    test "axent def with :error case" do
+      Code.compile_string(@test_error)
+      assert AxentDefTest.Test.test() == :error
+    end
   end
 
-  test "axent def with warning about implicit :ok" do
-    assert capture_io(fn ->
-             Code.compile_string(@test_ok_with_warning)
-           end) =~ "Implicit return value :ok"
-
-    assert AxentDefTest.Test.test_warning_ok() == :ok
-  end
-
-  test "axent def with :error case" do
-    Code.compile_string(@test_error)
-    assert AxentDefTest.Test.test_error() == :error
-  end
-
-  test "standard def behavior preserved" do
-    Code.compile_string(@test_normal_def)
-    assert AxentDefTest.Test.normal_fn() == :original
-    assert AxentDefTest.Test.normal_fn_with_rescue() == :error
-    assert AxentDefTest.Test.normal_fn_with_rescue_and_catch() == :caught
+  describe "Kernel (default) behaviour" do
+    test "standard def behavior preserved" do
+      Code.compile_string(@test_normal_def)
+      assert AxentDefTest.Test.normal_fn() == :original
+      assert AxentDefTest.Test.normal_fn_with_rescue() == :error
+      assert AxentDefTest.Test.normal_fn_with_rescue_and_catch() == :caught
+    end
   end
 end
