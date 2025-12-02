465654e91c1c2d2813113471f4ec71ae3e2d785f
Stefan Schulze <stefan.andre.schulze@posteo.de>
2025-12-02 16:44:21 +0100

    Add credo

--- mix.exs
+++ mix.exs
@@ -35,7 +35,8 @@ defmodule Venom.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 end
